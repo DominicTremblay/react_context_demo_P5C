e635e331b1274b6324b9f8c171a414138fe579e9
Dominic Tremblay <dominictremblay24@gmail.com>
2024-11-17 15:41:54 -0500

    changer les icones des tabs

--- app/_layout.js
+++ app/_layout.js
@@ -1,16 +1,34 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Tabs } from 'expo-router'
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
+import Feather from '@expo/vector-icons/Feather'
+import Fontisto from '@expo/vector-icons/Fontisto'
 
 const Layout = () => {
   return (
     <Tabs>
-      <Tabs.Screen name="index" options={{ title: 'Accueil' }} />
-      <Tabs.Screen name="films" options={{ title: 'Collection de films' }} />
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Accueil',
+          tabBarIcon: ({ color }) => (
+            <Feather name="home" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="films"
+        options={{
+          title: 'Collection de films',
+          tabBarIcon: ({ color }) => (
+            <Fontisto name="film" size={24} color={color} />
+          ),
+        }}
+      />
     </Tabs>
-  );
-};
+  )
+}
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({})
 
-export default Layout;
+export default Layout
---
+++ data/films.js
@@ -0,0 +1,77 @@
+export const films = [
+  {
+    annee_sortie: 2023,
+    description:
+      'Barbie d\u00e9couvre le monde r\u00e9el en quittant Barbieland.',
+    genre: 'Comedie',
+    id: 1,
+    image_url:
+      'https://m.media-amazon.com/images/M/MV5BYjI3NDU0ZGYtYjA2YS00Y2RlLTgwZDAtYTE2YTM5ZjE1M2JlXkEyXkFqcGc@._V1_.jpg',
+    titre: 'Barbie',
+  },
+  {
+    annee_sortie: 2023,
+    description:
+      "L'histoire du cr\u00e9ateur de la bombe atomique, J. Robert Oppenheimer.",
+    genre: 'Drame',
+    id: 2,
+    image_url:
+      'https://m.media-amazon.com/images/M/MV5BN2JkMDc5MGQtZjg3YS00NmFiLWIyZmQtZTJmNTM5MjVmYTQ4XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg',
+    titre: 'Oppenheimer',
+  },
+  {
+    annee_sortie: 2023,
+    description: 'Ethan Hunt doit stopper une nouvelle menace mondiale.',
+    genre: 'Action',
+    id: 3,
+    image_url: 'https://m.media-amazon.com/images/I/91dYe4m4XXL.jpg',
+    titre: 'Mission: Impossible \u2013 Dead Reckoning Part One',
+  },
+  {
+    annee_sortie: 2024,
+    description: 'Paul Atreides continue sa qu\u00eate sur Arrakis.',
+    genre: 'Sci-Fi',
+    id: 4,
+    image_url:
+      'https://m.media-amazon.com/images/M/MV5BZTA3NzY0ZjItNTk2OC00NTlkLWEzMzktZmFkOWJjMWE0MmQzXkEyXkFqcGc@._V1_.jpg',
+    titre: 'Dune: Part Two',
+  },
+  {
+    annee_sortie: 2024,
+    description: 'Aquaman affronte une nouvelle menace pour son royaume.',
+    genre: 'Action',
+    id: 5,
+    image_url:
+      'https://m.media-amazon.com/images/M/MV5BYjQ1ZTUzMWMtY2VkNS00ZDRjLWEwODYtYmFkMWJiNTQxMDUzXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg',
+    titre: 'Aquaman and the Lost Kingdom',
+  },
+  {
+    annee_sortie: 2023,
+    description: 'Miles Morales explore le multivers des Spider-Men.',
+    genre: 'Animation',
+    id: 6,
+    image_url:
+      'https://m.media-amazon.com/images/M/MV5BNThiZjA3MjItZGY5Ni00ZmJhLWEwN2EtOTBlYTA4Y2E0M2ZmXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg',
+    titre: 'Spider-Man: Across the Spider-Verse',
+  },
+  {
+    annee_sortie: 2023,
+    description:
+      'Les Marvels unissent leurs forces contre une menace cosmique.',
+    genre: 'Action',
+    id: 7,
+    image_url:
+      'https://m.media-amazon.com/images/M/MV5BYzczOWM4MzItMWMyOS00ZDczLWIxMzctNzBmYTgzOTI1MzI3XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg',
+    titre: 'The Marvels',
+  },
+  {
+    annee_sortie: 2023,
+    description:
+      "Les Gardiens se lancent dans une mission pour sauver l'un des leurs.",
+    genre: 'Action',
+    id: 8,
+    image_url:
+      'https://m.media-amazon.com/images/M/MV5BOTJhOTMxMmItZmE0Ny00MDc3LWEzOGEtOGFkMzY4MWYyZDQ0XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg',
+    titre: 'Guardians of the Galaxy Vol. 3',
+  },
+]
