2029adb9cbb184f2bf14ba8811a10dc4a948d8b2
Dominic Tremblay <dominictremblay24@gmail.com>
2024-11-25 11:15:10 -0500

    deplacer l'ouverture de session depuis _layout

--- app/_layout.js
+++ app/_layout.js
@@ -1,37 +1,61 @@
 import { Tabs } from 'expo-router'
-import React from 'react'
+import React, { useContext } from 'react'
 import { StyleSheet, View } from 'react-native'
 import Feather from '@expo/vector-icons/Feather'
 import Fontisto from '@expo/vector-icons/Fontisto'
-import { StateProvider } from '../context/StateContext'
+import { StateContext, StateProvider } from '../context/StateContext'
+import MessageErreur from '../components/messageErreur'
+import OuvrirSession from './session'
 
 const Layout = () => {
+  const { state, dispatch } = useContext(StateContext)
+
+  const {
+    auth: { estAuthentifie },
+  } = state
+
+  console.log(state)
+
+  if (!estAuthentifie) {
+    return (
+      <>
+        <MessageErreur message="Vous devez vous authentifier">
+          <OuvrirSession />
+        </MessageErreur>
+      </>
+    )
+  }
+
   return (
-    <StateProvider>
-      <Tabs>
-        <Tabs.Screen
-          name="index"
-          options={{
-            title: 'Accueil',
-            tabBarIcon: ({ color }) => (
-              <Feather name="home" size={24} color={color} />
-            ),
-          }}
-        />
-        <Tabs.Screen
-          name="films"
-          options={{
-            title: 'Collection de films',
-            tabBarIcon: ({ color }) => (
-              <Fontisto name="film" size={24} color={color} />
-            ),
-          }}
-        />
-      </Tabs>
-    </StateProvider>
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Accueil',
+          tabBarIcon: ({ color }) => (
+            <Feather name="home" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="films"
+        options={{
+          title: 'Collection de films',
+          tabBarIcon: ({ color }) => (
+            <Fontisto name="film" size={24} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
   )
 }
 
+const EtatEncapsuler = () => (
+  <StateProvider>
+    <Layout />
+  </StateProvider>
+)
+
 const styles = StyleSheet.create({})
 
-export default Layout
+export default EtatEncapsuler
--- app/index.js
+++ app/index.js
@@ -2,27 +2,9 @@ import { useContext } from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
 import { StateContext } from '../context/StateContext'
-import MessageErreur from '../components/messageErreur'
-import OuvrirSession from './session'
 
 export default function App() {
   const { state, dispatch } = useContext(StateContext)
-
-  const {
-    auth: { estAuthentifie },
-  } = state
-
-  console.log(state)
-
-  if (!estAuthentifie) {
-    return (
-      <>
-        <MessageErreur message="Vous devez vous authentifier">
-          <OuvrirSession />
-        </MessageErreur>
-      </>
-    )
-  }
 
   return (
     <View style={styles.container}>
